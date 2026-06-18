93001ea811e68d9cd344d15b6080aa22a99558c0
blakewhitney3163 <blakewhitney3163@gmail.com>
2026-06-18 08:42:49 -0700

    Update types.ts

--- src/schedule/types.ts
+++ src/schedule/types.ts
@@ -22,5 +22,6 @@ export interface BoxScorePlayer {
   rush_attempts: number; rush_yards: number; rush_tds: number;
   receptions: number; rec_yards: number; rec_tds: number; targets: number;
   tackles: number; assisted_tackles: number; sacks: number; tfl: number;
-  def_interceptions: number; pass_deflections: number;
+  def_interceptions: number; pass_deflections: number; def_tds: number;
+  fg_made: number; fg_att: number; xp_made: number; xp_att: number;
 }
