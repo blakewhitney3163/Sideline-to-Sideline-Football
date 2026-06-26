c3828eea30fa1632e1d5c7272bd6478bde6b9b86
blakewhitney3163 <blakewhitney3163@gmail.com>
2026-06-26 16:21:10 -0700

    Update types.ts

--- src/schedule/types.ts
+++ src/schedule/types.ts
@@ -1,3 +1,12 @@
+export interface Game {
+  id: number;
+  week: number;
+  home_team: string;
+  away_team: string;
+  home_score: number;
+  away_score: number;
+}
+
 export interface BoxScoreGame {
   id: number; week: number;
   home_team: string; away_team: string;
